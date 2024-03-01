88f466574d5046a2d12396583753bbcd9be04865
thenameajay <thenameajay@gmail.com>
2024-03-01 17:46:17 +0530

    deployment error correction 13

--- Backend/Server.js
+++ Backend/Server.js
@@ -16,7 +16,7 @@ app.use(
 app.use(express.json());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(cors())
+app.use(cors({origin:'https://weatheringtoyou.onrender.com'}))
 
 
 // -------------------------------------------
